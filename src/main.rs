0e4afa3bb725970178dbb64ac9afea24727e8d7b
Platon Floria <platon.floria@gmail.com>
2022-12-01 23:25:45 +0800

    feat: cache added

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use std::{env, time::SystemTime};
+use std::{env, time::SystemTime, collections::HashMap};
 
 use ethereum_abi::{Abi, Value};
 use hex_literal::hex;
 use num_bigfloat::BigFloat;
-use tokio::{sync::mpsc::{self, Sender, Receiver}};
+use tokio::sync::{mpsc::{self, Sender, Receiver}, Mutex};
 use web3::{
     futures::StreamExt,
     transports::WebSocket,
@@ -32,6 +32,11 @@ fn convert_q64_96(q64_96: ethereum_types::U256) -> BigFloat {
     ) / bf2.pow(&bf96)
 }
 
+enum PoolType {
+    V2,
+    V3,
+}
+
 struct Controller {
     web3: web3::Web3<WebSocket>,
     erc20_contract: ethabi::Contract,
@@ -39,6 +44,8 @@ struct Controller {
     uniswap_v2_abi: Abi,
     uniswap_v3_contract: ethabi::Contract,
     uniswap_v3_abi: Abi,
+    pool_info_cache: Mutex<HashMap<Address, (String, u32, u32)>>,
+    erc20_info_cache: Mutex<HashMap<Address, (String, u32)>>,
 }
 
 impl Controller {
@@ -50,6 +57,8 @@ impl Controller {
             uniswap_v2_abi: serde_json::from_str(include_str!("../resources/IUniswapV2Pair.abi")).unwrap(),
             uniswap_v3_contract: serde_json::from_str(include_str!("../resources/IUniswapV3Pair.abi")).unwrap(),
             uniswap_v3_abi: serde_json::from_str(include_str!("../resources/IUniswapV3Pair.abi")).unwrap(),
+            pool_info_cache: Mutex::new(HashMap::new()),
+            erc20_info_cache: Mutex::new(HashMap::new()),
         }
     }
 
@@ -72,7 +81,7 @@ impl Controller {
         sub.fold(log_sender, |log_sender, log| async move {
             if let Ok(log) = log {
                 let timestamp = SystemTime::now();
-                log_sender.send((timestamp, log)).await;
+                log_sender.send((timestamp, log)).await.expect("failed to push log");
             }
 
             log_sender
@@ -84,89 +93,130 @@ impl Controller {
     async fn process_logs(&self, mut log_receiver: Receiver<(SystemTime, Log)>) -> web3::contract::Result<()> {
         loop {
             if let Some((timestamp, log)) = log_receiver.recv().await {
-                let block_number = log.block_number.unwrap();
-                let transaction_hash = log.transaction_hash.unwrap();
-                let pool_contract_address = log.address;
-                let (pool_type, symbol, price) =  match hex::encode(log.topics[0].as_bytes()).as_str() {
-                    "1c411e9a96e071241c2f21f7726b17ae89e3cab4c78be50e062b03a9fffbbad1" => {
-                        let (symbol, decimals0, decimals1) = self.get_uniswap_v2_pool_info(pool_contract_address).await?;
-
-                        let topics: Vec<_> = log.topics.iter().map(|&t| ethereum_types::H256::from_slice(&t.to_fixed_bytes())).collect();
-                        let (_, log_data) = self.uniswap_v2_abi
-                            .decode_log_from_slice(&topics, &log.data.0)
-                            .expect("failed decoding log");
-                        let reserve0 = match log_data.reader().by_name.get("reserve0").unwrap().value {
-                            Value::Uint(value, _) => value.as_u128(),
-                            _ => 0
-                        };
-                        let reserve1 = match log_data.reader().by_name.get("reserve1").unwrap().value {
-                            Value::Uint(value, _) => value.as_u128(),
-                            _ => 0
-                        };
-                        let price = (BigFloat::from(reserve1) / BigFloat::from(reserve0) / BigFloat::from(10).pow(&BigFloat::from(decimals1 as i32 - decimals0 as i32))).to_f64();
-                        Some(("V2", symbol, price))
-                    },
-                    "c42079f94a6350d7e6235f29174924f928cc2ac818eb64fed8004e115fbcca67" => {
-                        let (symbol, decimals0, decimals1) = self.get_uniswap_v3_pool_info(pool_contract_address).await?;
-
-                        let topics: Vec<_> = log.topics.iter().map(|&t| ethereum_types::H256::from_slice(&t.to_fixed_bytes())).collect();
-                        let (_, log_data) = self.uniswap_v3_abi
-                            .decode_log_from_slice(&topics, &log.data.0)
-                            .expect("failed decoding log");
-                        let price = match log_data.reader().by_name.get("sqrtPriceX96").unwrap().value {
-                            Value::Uint(value, _) => {
-                                (convert_q64_96(value).pow(&BigFloat::from(2)) / BigFloat::from(10).pow(&BigFloat::from(decimals1 as i32 - decimals0 as i32))).to_f64()
-                            },
-                            _ => 0.0
-                        };
-
-                        Some(("V3", symbol, price))
-                    },
-                    _ => None
-                }.unwrap();
-
-                println!(
-"Received timestamp: {}
+                match self.process_log(&log).await {
+                    Ok((block_number, transaction_hash, pool_contract_address, pool_type, symbol, price)) => {
+                        println!(
+                            "
+Received timestamp: {}
 Block number: {}
 Transaction hash: 0x{}
 Pool contract address: 0x{}
 Pool type: {}
 Token's pair: {}
-New price: {}",
-                    timestamp.duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs(),
-                    block_number,
-                    hex::encode(transaction_hash.as_bytes()).as_str(),
-                    hex::encode(pool_contract_address.as_bytes()).as_str(),
-                    pool_type,
-                    symbol,
-                    price
-                );
+New price: {}
+                            ",
+                            timestamp.duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs(),
+                            block_number,
+                            transaction_hash,
+                            pool_contract_address,
+                            match pool_type {PoolType::V2 => "V2", PoolType::V3 => "V3"},
+                            symbol,
+                            price
+                        );
+                    },
+                    Err(_) => {
+                        println!("failed to process the log\n{:?}", log);
+                    }
+                }
             }
         }
     }
 
+    async fn process_log(&self, log: &Log) -> web3::contract::Result<(
+        u64,
+        String,
+        String,
+        PoolType,
+        String,
+        f64,
+    )> {
+        let block_number = log.block_number.unwrap();
+        let transaction_hash = log.transaction_hash.unwrap();
+        let pool_contract_address = log.address;
+        let topic = hex::encode(log.topics[0].as_bytes());
+        let (pool_type, symbol, price) =  match topic.as_str() {
+            "1c411e9a96e071241c2f21f7726b17ae89e3cab4c78be50e062b03a9fffbbad1" => {
+                let (symbol, decimals0, decimals1) = self.get_uniswap_v2_pool_info(pool_contract_address).await?;
+
+                let topics: Vec<_> = log.topics.iter().map(|&t| ethereum_types::H256::from_slice(&t.to_fixed_bytes())).collect();
+                let (_, log_data) = self.uniswap_v2_abi
+                    .decode_log_from_slice(&topics, &log.data.0)
+                    .expect("failed decoding log");
+                let reserve0 = match log_data.reader().by_name.get("reserve0").unwrap().value {
+                    Value::Uint(value, _) => value.as_u128(),
+                    _ => 0
+                };
+                let reserve1 = match log_data.reader().by_name.get("reserve1").unwrap().value {
+                    Value::Uint(value, _) => value.as_u128(),
+                    _ => 0
+                };
+                let price = (BigFloat::from(reserve1) / BigFloat::from(reserve0) / BigFloat::from(10).pow(&BigFloat::from(decimals1 as i32 - decimals0 as i32))).to_f64();
+                Some((PoolType::V2, symbol, price))
+            },
+            "c42079f94a6350d7e6235f29174924f928cc2ac818eb64fed8004e115fbcca67" => {
+                let (symbol, decimals0, decimals1) = self.get_uniswap_v3_pool_info(pool_contract_address).await?;
+
+                let topics: Vec<_> = log.topics.iter().map(|&t| ethereum_types::H256::from_slice(&t.to_fixed_bytes())).collect();
+                let (_, log_data) = self.uniswap_v3_abi
+                    .decode_log_from_slice(&topics, &log.data.0)
+                    .expect("failed decoding log");
+                let price = match log_data.reader().by_name.get("sqrtPriceX96").unwrap().value {
+                    Value::Uint(value, _) => {
+                        (convert_q64_96(value).pow(&BigFloat::from(2)) / BigFloat::from(10).pow(&BigFloat::from(decimals1 as i32 - decimals0 as i32))).to_f64()
+                    },
+                    _ => 0.0
+                };
+
+                Some((PoolType::V3, symbol, price))
+            },
+            _ => None
+        }.unwrap();
+        Ok((
+            block_number.as_u64(),
+            hex::encode(transaction_hash.as_bytes()),
+            hex::encode(pool_contract_address.as_bytes()),
+            pool_type,
+            symbol,
+            price
+        ))
+    }
+
     async fn get_uniswap_v2_pool_info(&self, address: Address) -> web3::contract::Result<(String, u32, u32)> {
+        if let Some((symbol, decimals0, decimals1)) = self.pool_info_cache.lock().await.get(&address) {
+            return Ok((symbol.clone(), *decimals0, *decimals1));
+        }
         let contract = Contract::new(self.web3.eth(), address, self.uniswap_v2_contract.clone());
         let token0_address: Address = contract.query("token0", (), None, Options::default(), None).await?;
         let (symbol0, decimals0) = self.get_erc20_info(token0_address).await?;
         let token1_address: Address = contract.query("token1", (), None, Options::default(), None).await?;
         let (symbol1, decimals1) = self.get_erc20_info(token1_address).await?;
-        Ok((format!("{}/{}", symbol0, symbol1), decimals0, decimals1))
+        let symbol = format!("{}/{}", symbol0, symbol1);
+        self.pool_info_cache.lock().await.insert(address, (symbol.clone(), decimals0, decimals1));
+        Ok((symbol, decimals0, decimals1))
     }
 
     async fn get_uniswap_v3_pool_info(&self, address: Address) -> web3::contract::Result<(String, u32, u32)> {
+        if let Some((symbol, decimals0, decimals1)) = self.pool_info_cache.lock().await.get(&address) {
+            return Ok((symbol.clone(), *decimals0, *decimals1));
+        }
         let contract = Contract::new(self.web3.eth(), address, self.uniswap_v3_contract.clone());
         let token0_address: Address = contract.query("token0", (), None, Options::default(), None).await?;
         let (symbol0, decimals0) = self.get_erc20_info(token0_address).await?;
         let token1_address: Address = contract.query("token1", (), None, Options::default(), None).await?;
         let (symbol1, decimals1) = self.get_erc20_info(token1_address).await?;
-        Ok((format!("{}/{}", symbol0, symbol1), decimals0, decimals1))
+        let symbol = format!("{}/{}", symbol0, symbol1);
+        self.pool_info_cache.lock().await.insert(address, (symbol.clone(), decimals0, decimals1));
+        Ok((symbol, decimals0, decimals1))
     }
 
     async fn get_erc20_info(&self, address: Address) -> web3::contract::Result<(String, u32)> {
+        if let Some((symbol, decimals)) = self.erc20_info_cache.lock().await.get(&address) {
+            return Ok((symbol.clone(), *decimals))
+        }
         let contract = Contract::new(self.web3.eth(), address, self.erc20_contract.clone());
         let symbol: String = contract.query("symbol", (), None, Options::default(), None).await?;
         let decimals: u32 = contract.query("decimals", (), None, Options::default(), None).await?;
+        self.erc20_info_cache.lock().await.insert(address, (symbol.clone(), decimals));
         Ok((symbol, decimals))
     }
 }
